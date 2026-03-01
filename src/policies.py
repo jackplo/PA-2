1e3c084ca87dc477667f157011fa7eef44fc7919
Jack Lohse <jackplo53@gmail.com>
2026-03-01 14:06:22 -0500

    FIFO Cache Policy

--- src/policies.py
+++ src/policies.py
@@ -1,8 +1,35 @@
-def fifo(k: int, m: int, rm: list[int]):
-    pass
+from abc import ABC, abstractmethod
+from collections import deque
 
-def lru(k: int, m: int, rm: list[int]):
-    pass
+class BaseCache(ABC):
 
-def optff(k: int, m: int, rm: list[int]):
-    pass
+    def __init__(self, k: int):
+        self.k = k
+        self.misses = 0
+
+    @abstractmethod
+    def get(self, id: int) -> bool:
+        pass
+
+    def get_miss_count(self) -> int:
+        return self.misses
+    
+class FIFOCache(BaseCache):
+
+    def __init__(self, k: int):
+        super().__init__(k)
+        self.cache: deque[int] = deque()
+        self.uniques: set[int] = set()
+
+    def get(self, id: int) -> bool:
+        if id not in self.uniques:
+            self.misses += 1
+            if len(self.cache) >= self.k:
+                evictee = self.cache.popleft()
+                self.uniques.remove(evictee)
+           
+            self.cache.append(id)
+            self.uniques.add(id)
+            return False
+        else:
+            return True
