6b48ddd192ae8ce0c43f1b4f2436917fc4f4a4f5
Jack Lohse <jackplo53@gmail.com>
2026-03-01 13:19:08 -0500

    IO

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from src.util.io import read_input_file
+
 def main():
-    print("Hello from pa-2!")
+    k, m, rm = read_input_file("data/1.in")
+    print(k, m, rm)
 
 
 if __name__ == "__main__":
---
+++ src/util/io.py
@@ -0,0 +1,11 @@
+def read_input_file(file_name: str) -> list[int, int, list[int]]:
+    with open(file_name, 'r') as file:
+        km = file.readline().strip()
+
+        k, m = int(km[0]), int(km[2])
+
+        rm_str = file.readline().strip().split(" ")
+        rm = [int(r) for r in rm_str]
+
+    return [k, m, rm]
+    
